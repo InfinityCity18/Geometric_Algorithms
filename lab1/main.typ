f9f3b7fe63831b1218532aad5dd8e7b90f5365e9
Jakub <kubawlas1@gmail.com>
2025-10-21 22:07:27 +0200

    progress

--- lab1/main.typ
+++ lab1/main.typ
@@ -1,7 +1,133 @@
+#set text(lang: "pl", size: 12pt)
+#set par(justify: true)
+#set heading(numbering: "1.")
+#set grid(column-gutter: 1em)
+#set table(align: center + horizon)
+#set page(numbering: "1")
+#show table: set par(justify: false)
+#show table.cell.where(y: 0): strong
+
+#line(length: 100%)
+
+#align(center)[
+  #text(size: 2.5em)[*Algorytmy Geometryczne*] \
+
+  #text(size: 1.6em)[*Ćwiczenie 1 - Predykaty geometryczne*] \
+
+  #text(size: 1.2em)[Jakub Własiewicz - Grupa 2]
+]
+
+= Opis ćwiczenia
+
+Ćwiczenie polegało na klasyfikacji położenia punktów względem prostej na położone po jej lewej, prawej oraz współliniowe z nią,
+a następnie wizualizacji wyników i opisania wniosków.
+Do analizy zostały dane następujące zbiory:
+- *Zbiór A* : $10^5$ punktów z płaszczyzny $ [-1000, 1000]^2$
+- *Zbiór B* : $10^5$ punktów z płaszczyzny $ [-10^14, 10^14]^2$
+- *Zbiór C* : $1000$ punktów spełniających równanie okręgu o środku $(0,0)$ i promieniu \ $R = 100$
+- *Zbiór D* : $1000$ punktów z przedziału $[-1000,1000]$ znajdujących się na prostej tworzonej przez wektor $accent(a b, arrow)$, gdzie $a = (-1,0)$, $b = (1, 0.1)$
+
+= Dane techniczne
+Program został uruchomiony na komputerze z następującymi specyfikacjami:
+- *System Operacyjny -* Ubuntu 24.04.3 LTS
+- *Architektura Procesora -* x86_64
+- *Procesor -* AMD Ryzen 7 7840HS
+- *Język i wersja kompilatora -* Rust 1.86.0
+
+Oraz użyte zostały następujące biblioteki:
+- *plotters 0.3.7*
+- *rand 0.9.2*
+
+= Opis teoretyczny
+Aby wyznaczyć położenie punktu $c$ względem prostej danej punktami $a$ i $b$, analizujemy wartość wyznacznika macierzy 2x2 lub 3x3 danym następującymi wzorami:
+#align(center)[
+#grid(
+  columns: (auto, auto, auto),
+  align: horizon
+)[
+$ det(a,b,c) = 
+    mat(delim: "|", a_x, a_y, 0;
+        b_x, b_y, 0;
+        c_x, b_y, 0;
+
+    ) 
+  $
+][oraz][
+  #v(1.8pt)
+  $ 
+    det(a,b,c) = mat(delim: "|", a_x - c_x, a_y - c_y; b_x - c_x, b_y - c_y
+    ) 
+  $
+]
+]
+
+$ det(a,b,c) = cases(
+  > 0 - "punkt jest po lewej stronie prostej",
+  < 0 - "punkt jest po prawej stronie prostej",
+  = 0 - "punkt jest współliniowy"
+) $
+
+#grid(
+  columns: (auto,auto)
+)[
+  Dzięki temu możemy dokonać obliczeń,
+  ale ze względu na właściwości arytmetyki
+  zmienno-przecinkowej, nie zalecane jest
+  bezpośrednie przyrówanie wyznacznika do zera.
+
+  Jedną z możliwości jest przyrównanie
+  modułu różnicy liczby z zerem do jakiegoś
+  małego $epsilon$ :
+  $ abs(x - 0.0) <= epsilon $
+  Dla wszystkich obliczeń poniżej przyjmiemy $epsilon = 10^(-16)$.
+][
 #figure(
-  image("bajojajo.svg", width: 40%),
-  caption: [
-    A step in the molecular testing
-    pipeline of our lab.
-  ],
+  image("prosta.png", width: 60%),
+  caption: [Punkt $c$ i prosta wyznaczona przez $a,b$. Punkt $c$ znajduje się po lewej stronie.]
 )
+]
+
+= Realizacja zadania
+
+Na wszystkich poniższych wizualizacjach do oznaczenia położenia punktów względem prostej zostały użyte następujące kolory:
+- #text(blue)[*Niebieski*] - dla punktów współliniowych,
+- #text(red)[*Czerwony*] - dla punktów po prawej,
+- #text(green)[*Zielony*] - dla punktów po lewej
+
+Oraz następujące oznaczenia dla tablic:
+- *f32* - liczby zmienno-przecinkowe 32-bitowe
+- *f64* - liczby zmienno-przecinkowe 64-bitowe
+- *det_3x3* - wyznacznik macierzy 3x3
+- *det_2x2* - wyznacznik macierzy 2x2
+
+== Zbiór A
+#grid(columns: (50%, 50%), rows: (auto))[
+#figure(
+  caption: [Zbiór A - $10^5$ punktów o współrzędnych z przedziału $[-1000, 1000]$],
+  image("plots_typst/1_up.png", width: 90%)
+)][
+#figure(
+  caption: [Zbiór A po pokolorowaniu punktów według kryterium],
+  image("plots_typst/1_p.png", width: 90%)
+)
+]
+
+#figure(
+  caption: [Wyniki klasyfikacji zbioru A],
+  table(
+    table.header(
+      [Typ liczb zmienno-przecinkowych],
+      [Rodzaj wyznacznika],
+      [Liczba punktów po lewej],
+      [Liczba punktów po prawej],
+      [Liczba punktów współliniowych],
+    ),
+    columns: 5,
+    rows: 4,
+    table.cell(rowspan: 2)[f32], [det_2x2], [50276], [49724], [0],
+    [det_3x3], [50276], [49724], [0],
+    table.cell(rowspan: 2)[f64], [det_2x2], [50276], [49724], [0],
+    [det_2x2], [50276], [49724], [0]
+  )
+)
+W tym zbiorze, niezależnie od doboru precyzji czy wyznacznika, otrzymujemy te same rezultaty. Zostało to przedstawione w Tabeli 1.
--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -7,8 +7,10 @@ use rand::{
 use std::ops::Range;
 
 type Float = f64;
+const PI: Float = std::f64::consts::PI;
 const A: (Float, Float) = (-1.0, 0.0);
 const B: (Float, Float) = (1.0, 0.1);
+const EPSILON: Float = 1e-16;
 
 const SEED_X_1: u64 = 117;
 const SEED_Y_1: u64 = 2139;
@@ -20,14 +22,15 @@ const SEED_R_3: u64 = 9214;
 
 const SEED_X_4: u64 = 1321;
 
+
 fn main() {
-    
     { // zbiór 1
         use problemset1::*;
         let range = -1000.0..1000.0;
         let points = new_points(range.clone(), 100000, SEED_X_1, SEED_Y_1);
-        draw_unprocessed("plots/1_up.svg", points.clone(), -1200.0..1200.0).unwrap();
-        let (l, r, colin) = draw_processed("plots/1_p.svg", points, -1200.0..1200.0, 1e-14, det_2x2).unwrap();
+        draw_unprocessed("plots/1_up.png", points.clone(), -1200.0..1200.0).unwrap();
+        let (l, r, colin) = draw_processed("plots/1_p.png", points, -1200.0..1200.0, EPSILON, det_3x3).unwrap();
+        println!("SET 1");
         println!("Points on the left: {}", l);
         println!("Points on the right: {}", r);
         println!("Collinear points: {}", colin);
@@ -37,8 +40,9 @@ fn main() {
         use problemset2::*;
         let range = -1e14..1e14;
         let points = new_points(range.clone(), 100000, SEED_X_2, SEED_Y_2);
-        draw_unprocessed("plots/2_up.svg", points.clone(), -1e14..1e14).unwrap();
-        let (l, r, colin) = draw_processed("plots/2_p.svg", points, -1e14..1e14, 1e-14, det_2x2).unwrap();
+        draw_unprocessed("plots/2_up.png", points.clone(), -1e14-2e13..1e14+2e13).unwrap();
+        let (l, r, colin) = draw_processed("plots/2_p.png", points, -1e14-2e13..1e14+2e13, EPSILON, det_3x3).unwrap();
+        println!("SET 2");
         println!("Points on the left: {}", l);
         println!("Points on the right: {}", r);
         println!("Collinear points: {}", colin);
@@ -48,8 +52,9 @@ fn main() {
         use problemset3::*;
         let r = 100.0;
         let points = new_points_circle(r, 1000, SEED_R_3);
-        draw_unprocessed("plots/3_up.svg", points.clone(), -120.0..120.0).unwrap();
-        let (l, r, colin) = draw_processed("plots/3_p.svg", points, -120.0..120.0, 1e-14, det_2x2).unwrap();
+        draw_unprocessed("plots/3_up.png", points.clone(), -120.0..120.0).unwrap();
+        let (l, r, colin) = draw_processed("plots/3_p.png", points, -120.0..120.0, EPSILON, det_3x3).unwrap();
+        println!("SET 3");
         println!("Points on the left: {}", l);
         println!("Points on the right: {}", r);
         println!("Collinear points: {}", colin);
@@ -59,8 +64,9 @@ fn main() {
         use problemset4::*;
         let range= -1000.0..1000.0;
         let points = new_points_line(range, 1000, SEED_X_4);
-        draw_unprocessed("plots/4_up.svg", points.clone(), -1200.0..1200.0).unwrap();
-        let (l, r, colin) = draw_processed("plots/4_p.svg", points, -1200.0..1200.0, 1e-16, det_2x2).unwrap();
+        draw_unprocessed("plots/4_up.png", points.clone(), -1200.0..1200.0).unwrap();
+        let (l, r, colin) = draw_processed("plots/4_p.png", points, -1200.0..1200.0, EPSILON, det_3x3).unwrap();
+        println!("SET 4");
         println!("Points on the left: {}", l);
         println!("Points on the right: {}", r);
         println!("Collinear points: {}", colin);
@@ -72,7 +78,14 @@ fn det_3x3(a: (Float, Float), b: (Float, Float), c: (Float, Float)) -> Float {
 }
 
 fn det_2x2(a: (Float, Float), b: (Float, Float), c: (Float, Float)) -> Float {
-    (a.0 - c.0) * (b.1 - c.1) - (b.0 - c.0) * (a.1 - c.1)
+    //(a.0 - c.0) * (b.1 - c.1) - (b.0 - c.0) * (a.1 - c.1)
+    let x = a.0 - c.0;
+    let y = b.1 -c.1;
+    let z = b.0 - c.0;
+    let w = a.1 - c.1;
+    let xy = x * y;
+    let zw = z * w;
+    return xy - zw;
 }
 
 fn eq_float(a: Float, b: Float, epsilon: Float) -> bool {
@@ -82,7 +95,7 @@ fn eq_float(a: Float, b: Float, epsilon: Float) -> bool {
 mod problemset1 {
     use super::*;
     pub fn draw_unprocessed(filename: &str, points: Vec<(Float, Float)>, label_range: Range<Float>) -> Result<(), Box<dyn std::error::Error>> {
-        let root = SVGBackend::new(filename, (512, 512)).into_drawing_area();
+        let root = BitMapBackend::new(filename, (512, 512)).into_drawing_area();
         root.fill(&WHITE).unwrap();
 
         let areas = root.split_by_breakpoints([488], [24]);
@@ -104,14 +117,14 @@ mod problemset1 {
 
         ctx.draw_series(
             points.iter()
-                .map(|(x, y)| Circle::new((*x, *y), 2, BLUE.filled())),
+                .map(|(x, y)| Circle::new((*x, *y), 2, BLACK.filled())),
         )?;
         root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
         Ok(())
     }
 
     pub fn draw_processed(filename: &str, points: Vec<(Float, Float)>, label_range: Range<Float>, epsilon: Float, det: fn((Float, Float), (Float, Float), (Float, Float)) -> Float) -> Result<(usize, usize, usize), Box<dyn std::error::Error>> {
-        let root = SVGBackend::new(filename, (512, 512)).into_drawing_area();
+        let root = BitMapBackend::new(filename, (512, 512)).into_drawing_area();
         root.fill(&WHITE).unwrap();
 
         let areas = root.split_by_breakpoints([488], [24]);
@@ -174,7 +187,7 @@ mod problemset1 {
 mod problemset2 {
     use super::*;
     pub fn draw_unprocessed(filename: &str, points: Vec<(Float, Float)>, label_range: Range<Float>) -> Result<(), Box<dyn std::error::Error>> {
-        let root = SVGBackend::new(filename, (512, 512)).into_drawing_area();
+        let root = BitMapBackend::new(filename, (512, 512)).into_drawing_area();
         root.fill(&WHITE).unwrap();
 
         let areas = root.split_by_breakpoints([488], [24]);
@@ -198,14 +211,14 @@ mod problemset2 {
 
         ctx.draw_series(
             points.iter()
-                .map(|(x, y)| Circle::new((*x, *y), 2, BLUE.filled())),
+                .map(|(x, y)| Circle::new((*x, *y), 2, BLACK.filled())),
         )?;
         root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
         Ok(())
     }
 
     pub fn draw_processed(filename: &str, points: Vec<(Float, Float)>, label_range: Range<Float>, epsilon: Float, det: fn((Float, Float), (Float, Float), (Float, Float)) -> Float) -> Result<(usize, usize, usize), Box<dyn std::error::Error>> {
-        let root = SVGBackend::new(filename, (512, 512)).into_drawing_area();
+        let root = BitMapBackend::new(filename, (512, 512)).into_drawing_area();
         root.fill(&WHITE).unwrap();
 
         let areas = root.split_by_breakpoints([488], [24]);
@@ -227,11 +240,13 @@ mod problemset2 {
             .label_style(("sans-serif", 22).into_font())
             .draw()?;
 
+        let A_off = (A.0 * 1e10, A.1 * 1e10);
+        let B_off = (B.0 * 1e10, B.1 * 1e10);
         let mut on_left = Vec::new();
         let mut on_right = Vec::new();
         let mut collinear = Vec::new();
         for point in points {
-            let determinant = det(A, B, point);
+            let determinant = det(A_off, B_off, point);
             if eq_float(determinant, 0.0, epsilon) {
                 collinear.push(point);
             } else if determinant < 0.0 {
@@ -270,7 +285,7 @@ mod problemset2 {
 mod problemset3 {
     use super::*;
     pub fn draw_unprocessed(filename: &str, points: Vec<(Float, Float)>, label_range: Range<Float>) -> Result<(), Box<dyn std::error::Error>> {
-        let root = SVGBackend::new(filename, (512, 512)).into_drawing_area();
+        let root = BitMapBackend::new(filename, (512, 512)).into_drawing_area();
         root.fill(&WHITE).unwrap();
 
         let areas = root.split_by_breakpoints([488], [24]);
@@ -292,14 +307,14 @@ mod problemset3 {
 
         ctx.draw_series(
             points.iter()
-                .map(|(x, y)| Circle::new((*x, *y), 2, BLUE.filled())),
+                .map(|(x, y)| Circle::new((*x, *y), 2, BLACK.filled())),
         )?;
         root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
         Ok(())
     }
 
     pub fn draw_processed(filename: &str, points: Vec<(Float, Float)>, label_range: Range<Float>, epsilon: Float, det: fn((Float, Float), (Float, Float), (Float, Float)) -> Float) -> Result<(usize, usize, usize), Box<dyn std::error::Error>> {
-        let root = SVGBackend::new(filename, (512, 512)).into_drawing_area();
+        let root = BitMapBackend::new(filename, (512, 512)).into_drawing_area();
         root.fill(&WHITE).unwrap();
 
         let areas = root.split_by_breakpoints([488], [24]);
@@ -350,7 +365,7 @@ mod problemset3 {
     }
 
     pub fn new_points_circle(r: Float, n: usize, seed_r: u64) -> Vec<(Float, Float)> {
-        let uni_dist = Uniform::new_inclusive(0.0, 2.0 * std::f64::consts::PI).unwrap();
+        let uni_dist = Uniform::new_inclusive(0.0, 2.0 * PI).unwrap();
         let mut r_rand = SmallRng::seed_from_u64(seed_r);
         let r_iter = uni_dist.sample_iter(&mut r_rand);
         r_iter.map(|theta| (r * theta.cos(), r * theta.sin())).take(n).collect()
@@ -360,15 +375,16 @@ mod problemset3 {
 mod problemset4 {
     use super::*;
     pub fn draw_unprocessed(filename: &str, points: Vec<(Float, Float)>, label_range: Range<Float>) -> Result<(), Box<dyn std::error::Error>> {
-        let root = SVGBackend::new(filename, (512, 512)).into_drawing_area();
+        let root = BitMapBackend::new(filename, (512, 512)).into_drawing_area();
         root.fill(&WHITE).unwrap();
 
         let areas = root.split_by_breakpoints([488], [24]);
+        let y_spec : Range<Float> = -70.0..70.0;
 
         let mut ctx = ChartBuilder::on(&areas[2])
             .x_label_area_size(80)
             .y_label_area_size(80)
-            .build_cartesian_2d(label_range.clone(), -70.0..70.0)?;
+            .build_cartesian_2d(label_range.clone(), y_spec)?;
         ctx
             .configure_mesh()
             .disable_x_mesh()
@@ -382,22 +398,23 @@ mod problemset4 {
 
         ctx.draw_series(
             points.iter()
-                .map(|(x, y)| Circle::new((*x, *y), 2, BLUE.filled())),
+                .map(|(x, y)| Circle::new((*x, *y), 2, BLACK.filled())),
         )?;
         root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
         Ok(())
     }
 
     pub fn draw_processed(filename: &str, points: Vec<(Float, Float)>, label_range: Range<Float>, epsilon: Float, det: fn((Float, Float), (Float, Float), (Float, Float)) -> Float) -> Result<(usize, usize, usize), Box<dyn std::error::Error>> {
-        let root = SVGBackend::new(filename, (512, 512)).into_drawing_area();
+        let root = BitMapBackend::new(filename, (512, 512)).into_drawing_area();
         root.fill(&WHITE).unwrap();
 
         let areas = root.split_by_breakpoints([488], [24]);
+        let y_spec : Range<Float> = -70.0..70.0;
 
         let mut ctx = ChartBuilder::on(&areas[2])
             .x_label_area_size(80)
             .y_label_area_size(80)
-            .build_cartesian_2d(label_range.clone(), -70.0..70.0)?;
+            .build_cartesian_2d(label_range.clone(), y_spec)?;
         ctx
             .configure_mesh()
             .disable_x_mesh()
@@ -422,7 +439,7 @@ mod problemset4 {
                 on_left.push(point);
             }
         }
-
+        
         ctx.draw_series(
             on_left.clone().iter()
                 .map(|(x, y)| Circle::new((*x, *y), 2, GREEN.filled())),
